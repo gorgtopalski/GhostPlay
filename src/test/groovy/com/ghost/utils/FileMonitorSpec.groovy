8dcdf51d72e5bf15c90464e428dface5900b3845
Gueorgui Topalski <gueorgui.tzvetoslavov@gmail.com>
2017-07-24 00:51:56 +0200

    test: the actual test for the FileMonitor

--- src/test/groovy/com/ghost/utils/FileMonitorSpec.groovy
+++ src/test/groovy/com/ghost/utils/FileMonitorSpec.groovy
@@ -1,4 +1,43 @@
 package com.ghost.utils
 
-class FileMonitorSpec {
+import spock.lang.Shared
+import spock.lang.Specification
+
+import static groovyx.gpars.actor.Actors.actor
+
+class FileMonitorSpec extends Specification
+{
+    @Shared
+    File file
+
+    def setupSpec()
+    {
+        file = new File('temp.txt')
+        file.createNewFile()
+    }
+
+    def "File monitoring"()
+    {
+        given:
+        def hasChanged = false
+        def notify = actor {
+            react { msg -> hasChanged = true }
+        }
+        def fm = new FileMonitorActor(notify, '.', 'temp.txt').start()
+
+        expect:
+        assert !hasChanged
+
+        when:
+        file.write('Random line')
+        notify.join()
+
+        then:
+        assert hasChanged
+    }
+
+    def cleanupSpec()
+    {
+        file.delete()
+    }
 }
